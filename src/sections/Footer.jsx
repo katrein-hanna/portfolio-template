b72088188e90cf2f647e17273537e29ef6cadd07
Katrein Hanna <katrein.hanna.96@gmail.com>
2024-04-30 01:00:00 +0300

    feat: implement contact form section with input validation

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-orange-100 text-sm text-secondary text-center p-5">
+      Made with ❤️... Happy Coding
+    </div>
+  );
+}
+
+export default Footer;
